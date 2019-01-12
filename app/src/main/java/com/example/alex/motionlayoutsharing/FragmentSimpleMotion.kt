bed4338595763b8b4d48cce3ffbe0bd03c503df5
Sasha Kryvoruchko <ogkryvoruchko@gmail.com>
2019-01-12 12:57:46 +0200

    add simple motion fragment

--- app/src/main/java/com/example/alex/motionlayoutsharing/FragmentSimpleMotion.kt
+++ app/src/main/java/com/example/alex/motionlayoutsharing/FragmentSimpleMotion.kt
@@ -1,8 +1,14 @@
 package com.example.alex.motionlayoutsharing
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class FragmentSimpleMotion : Fragment() {
 
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_simple_motion, container, false)
+    }
 }
