bfc5b396d15391851e3da618688cba59b4b00a52
Sasha Kryvoruchko <ogkryvoruchko@gmail.com>
2019-01-13 13:20:37 +0200

    refactored

--- app/src/main/java/com/example/alex/motionlayoutsharing/MainActivity.kt
+++ app/src/main/java/com/example/alex/motionlayoutsharing/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.alex.motionlayoutsharing
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
